3ed4b816fc00c981a8f88e4fe83ca4b2f7a2e77b
Salvatore <ssalvatoreeco@gmail.com>
2018-09-15 22:42:08 +0200

    Analysis query improvement

--- MAADB_twitter/resources/MongoDB/Query/ANALYSIS.sql
+++ MAADB_twitter/resources/MongoDB/Query/ANALYSIS.sql
@@ -1,13 +1,77 @@
-/*TWEET words trovati che non sono presenti tra i lexical resource = 197 su 200*/
-select dati1.word,t1.frequency from(
-select word from(
-select word
-from tweet
-where sentiment_fk = 1
-order by frequency desc
-)where rownum < 200
-minus
-select word
-from lexicalresource
-where sentiment_fk = 1) dati1, tweet t1
-where dati1.word = t1.word and t1.sentiment_fk =1;
+/*TWEET ANGER words trovati che non sono presenti tra i lexical resource = 190 su 200*/
+SELECT dati1.word,
+  t1.frequency
+FROM
+  (SELECT word
+  FROM
+    (SELECT T1.WORD,
+      T1.FREQUENCY
+    FROM TWEET T1
+    WHERE SENTIMENT_FK= 1
+    AND T1.WORD NOT  IN
+      (SELECT T2.WORD
+      FROM TWEET T2
+      WHERE T2.SENTIMENT_FK != T1.SENTIMENT_FK
+      AND T2.FREQUENCY       >80
+      )
+    ORDER BY FREQUENCY DESC
+    FETCH FIRST 200 ROWS ONLY
+    )
+  MINUS
+  SELECT word FROM lexicalresource WHERE sentiment_fk = 1
+  ) dati1,
+  tweet t1
+WHERE upper(dati1.word)    = upper(t1.word)
+AND t1.sentiment_fk =1;
+
+/*TWEET ANTICIPATION words trovati che non sono presenti tra i lexical resource = 200 su 200*/
+SELECT dati1.word,
+  t1.frequency
+FROM
+  (SELECT word
+  FROM
+    (SELECT T1.WORD,
+      T1.FREQUENCY
+    FROM TWEET T1
+    WHERE SENTIMENT_FK= 2
+    AND T1.WORD NOT  IN
+      (SELECT T2.WORD
+      FROM TWEET T2
+      WHERE T2.SENTIMENT_FK != T1.SENTIMENT_FK
+      AND T2.FREQUENCY       >80
+      )
+    ORDER BY FREQUENCY DESC
+    FETCH FIRST 200 ROWS ONLY
+    )
+  MINUS
+  SELECT word FROM lexicalresource WHERE sentiment_fk = 2
+  ) dati1,
+  tweet t1
+WHERE upper(dati1.word)    = upper(t1.word)
+AND t1.sentiment_fk =2;
+
+/*TWEET DISGUST words trovati che non sono presenti tra i lexical resource = 200 su 200*/
+SELECT dati1.word,
+  t1.frequency
+FROM
+  (SELECT word
+  FROM
+    (SELECT T1.WORD,
+      T1.FREQUENCY
+    FROM TWEET T1
+    WHERE SENTIMENT_FK= 3
+    AND T1.WORD NOT  IN
+      (SELECT T2.WORD
+      FROM TWEET T2
+      WHERE T2.SENTIMENT_FK != T1.SENTIMENT_FK
+      AND T2.FREQUENCY       >80
+      )
+    ORDER BY FREQUENCY DESC
+    FETCH FIRST 200 ROWS ONLY
+    )
+  MINUS
+  SELECT word FROM lexicalresource WHERE sentiment_fk = 3
+  ) dati1,
+  tweet t1
+WHERE upper(dati1.word)    = upper(t1.word)
+AND t1.sentiment_fk =3;
